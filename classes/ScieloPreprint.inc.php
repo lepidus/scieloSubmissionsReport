47ac0779f1ca8e23396f8ab8df847edc05c3b8f4
Pablo Valério Polônia <pablo@lepidus.com.br>
2021-04-23 17:45:07 -0300

    Finishes Preprint Submission properties. Issue: documentacao-e-tarefas/scielo#116
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Pablo <pablo@lepidus.com.br>

--- classes/ScieloPreprint.inc.php
+++ classes/ScieloPreprint.inc.php
@@ -6,29 +6,48 @@ class ScieloPreprint extends ScieloSubmission {
     private $sectionModerator;
     private $publicationStatus;
     private $publicationDOI;
+    private $notes;
 
-    public function __construct(int $id, string $title, string $submitter, string $dateSubmitted, int $daysUntilStatusChange, string $status, array $authors, string $section, string $language, string $finalDecision, string $finalDecisionDate, array $moderators, string $sectionModerator, string $publicationStatus, string $publicationDOI) {
+    public function __construct(int $id, string $title, string $submitter, string $dateSubmitted, int $daysUntilStatusChange, string $status, array $authors, string $section, string $language, string $finalDecision, string $finalDecisionDate, array $moderators, string $sectionModerator, string $publicationStatus, string $publicationDOI, array $notes) {
         parent::__construct($id, $title, $submitter, $dateSubmitted, $daysUntilStatusChange, $status, $authors, $section, $language, $finalDecision, $finalDecisionDate);
         $this->moderators = $moderators;
         $this->sectionModerator = $sectionModerator;
         $this->publicationStatus = $publicationStatus;
         $this->publicationDOI = $publicationDOI;
+        $this->notes = $notes;
     }
 
-    public function getModerators() : array {
-        return $this->moderators;
+    private function fillEmptyFields($field, $messageIfEmpty) {
+        if (empty($field))
+            return $messageIfEmpty;
+
+        return $field;
+    }
+
+    public function getModerators() : string {
+        if (empty($this->moderators))
+            return "No moderators";
+        
+        return implode(",", $this->moderators);
     }
 
     public function getSectionModerator() : string {
-        return $this->sectionModerator;
+        return $this->fillEmptyFields($this->sectionModerator, "No moderators");
     }
 
     public function getPublicationStatus() : string {
-        return $this->publicationStatus;
+        return $this->fillEmptyFields($this->publicationStatus, "No publication's status");
     }
 
     public function getPublicationDOI() : string {
-        return $this->publicationDOI;
+        return $this->fillEmptyFields($this->publicationDOI, "No publication's DOI");
+    }
+
+    public function getNotes() : string {
+        if(empty($this->notes))
+            return "No notes";
+        
+        return trim(preg_replace('/\s+/', ' ', "Note: " . implode(". Note: ", $this->notes)));
     }
 }
 
--- tests/ScieloPreprintTest.php
+++ tests/ScieloPreprintTest.php
@@ -8,28 +8,64 @@ class ScieloPreprintTest extends ScieloSubmissionTest {
     private $sectionModerator = "Clarice Lispector";
     private $publicationStatus = "Preprint has been published in a journal as an article";
     private $publicationDOI = "https://doi.org/10.1590/0100-3984.2020.53.2e1";
+    private $notes = array("The author forgot to cite relevant work on this preprint", "This work is wonderful! Congrats!");
 
     protected function createScieloSubmission() {
         parent::createScieloSubmission();
-        return new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, $this->sectionModerator, $this->publicationStatus, $this->publicationDOI);
+        return new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, $this->sectionModerator, $this->publicationStatus, $this->publicationDOI, $this->notes);
     }
 
     public function testHasModerators() : void {
-        $this->assertEquals($this->moderators, $this->submission->getModerators());
+        $this->assertEquals(implode(",", $this->moderators), $this->submission->getModerators());
+    }
+    
+    public function testHasNoModerators() : void {
+        $preprint = new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, [], $this->sectionModerator, $this->publicationStatus, $this->publicationDOI, $this->notes);
+        $this->assertEquals("No moderators", $preprint->getModerators());
     }
 
     public function testHasSectionModerator() : void {
         $this->assertEquals($this->sectionModerator, $this->submission->getSectionModerator());
     }
 
+    public function testHasNoSectionModerator() : void {
+        $preprint = new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, "", $this->publicationStatus, $this->publicationDOI, $this->notes);
+        $this->assertEquals("No moderators", $preprint->getSectionModerator());
+    }
+    
     public function testHasPublicationStatus() : void {
         $this->assertEquals($this->publicationStatus, $this->submission->getPublicationStatus());
     }
 
+    public function testWhenPublicationStatusIsEmpty() : void {
+        $preprint = new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, $this->sectionModerator, "", $this->publicationDOI, $this->notes);
+        $this->assertEquals("No publication's status", $preprint->getPublicationStatus());
+    }
+    
     public function testHasPublicationDOI() : void {
         $this->assertEquals($this->publicationDOI, $this->submission->getPublicationDOI());
     }
 
-    //testHasNotes
-}
+    public function testWhenPublicationDOIIsEmpty() : void {
+        $preprint = new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, $this->sectionModerator, $this->publicationStatus, "", $this->notes);
+        $this->assertEquals("No publication's DOI", $preprint->getPublicationDOI());
+    }
+
+    public function testHasNotes() : void {
+        $this->assertEquals("Note: " . implode(". Note: ", $this->notes), $this->submission->getNotes());
+    }
+
+    public function testWhenNotesHaveLinebreaks() : void {
+        $noteWithLineBreak = array("The author forgot to cite relevant work on this preprint.\nHe needs to cite more relevant works.");
+        $preprint = new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, $this->sectionModerator, $this->publicationStatus, $this->publicationDOI, $noteWithLineBreak);
+
+        $this->assertEquals("Note: The author forgot to cite relevant work on this preprint. He needs to cite more relevant works.", $preprint->getNotes());
+    }
     
+    public function testWhenPreprintHasNoNotes() : void {
+        $emptyNotes = array();
+        $preprint = new ScieloPreprint($this->submissionId, $this->title, $this->submitter, $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->authors, $this->section, $this->language, $this->finalDecision, $this->finalDecisionDate, $this->moderators, $this->sectionModerator, $this->publicationStatus, $this->publicationDOI, $emptyNotes);
+
+        $this->assertEquals("No notes", $preprint->getNotes());
+    }
+}
