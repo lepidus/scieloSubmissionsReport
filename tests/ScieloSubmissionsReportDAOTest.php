21dc46ce577d7cbb273963272c169bca73f4ff99
aline <aline@lepidus.com.br>
2021-05-03 17:36:35 -0400

    Adds database support to unit testing and factoring tests.
    Issue: documentacao-e-tarefas/scielo#152
    
    Signed-off-by: aline <aline@lepidus.com.br>
    Signed-off-by: thiago <thiago@lepidus.com.br>

--- tests/ScieloSubmissionsReportDAOTest.php
+++ tests/ScieloSubmissionsReportDAOTest.php
@@ -1,10 +1,12 @@
 <?php
 
-import('lib.pkp.tests.PKPTestCase');
+import('lib.pkp.tests.DatabaseTestCase');
 import('plugins.reports.ScieloSubmissionsReportPlugin.ScieloSubmissionsReportDAO');
+import('classes.submission.Submission');
+import('lib.pkp.classes.user.User');
 
 
-class ScieloSubmissionsReportDAOTest extends PKPTestCase {
+class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
 
 	private $application;
 	private $journalId;
@@ -12,47 +14,60 @@ class ScieloSubmissionsReportDAOTest extends PKPTestCase {
 	private $finalDecisionDate;
 	private $sessions;
 
-	protected function setUp() : void {
+	private $submissionId;
 
+	
+	protected function setUp() : void {
+		
 		$this->application = "ops";
 		$this->journalId = 1;
 		$this->initialDecisionDate = "2020-07-01";
 		$this->finalDecisionDate = "2020-07-31";
 		$this->sessions = ["Health Sciences"];
 		
+		$scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
+		DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
+		
+		parent::setUp();
+	}
+		
+	protected function getAffectedTables() {
+		return array('submissions','edit_decisions');
+	}
+	
+	public function testCreateSubmission(){
 		$submission = new Submission();
 		$submission->setContextId($this->journalId);
 		$submission->setStatus(STATUS_DECLINED);
-		//$submission->setSubmissionProgress(1);
+		$submission->setSubmissionProgress(1);
 		$submission->stampStatusModified();
-		$submission->setStageId(WORKFLOW_STAGE_ID_SUBMISSION);
+		$submission->setStageId(STATUS_DECLINED);
 		//$submission->setData('seriesId', $seriesId = current(array_keys($seriesOptions)));
 		//$submission->setLocale($this->getDefaultFormLocale());
+		
+		$submissionDao = DAORegistry::getDAO('SubmissionDAO'); 
+		
+		self::assertTrue(is_integer($this->submissionId = $submissionDao->insertObject($submission)));
+	}
 
+	public function testEditorDecisionSettings(){
 		$user = new User();
 		
 		$editorDecision = array(
-			'editDecisionId' => null,
-			'editorId' => $user->getId(),
-			'decision' => SUBMISSION_EDITOR_DECISION_DECLINE,
-			'dateDecided' => date(Core::getCurrentDate())
-		);
-
-		$scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
-		DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
-
-
-		$submissionDao = DAORegistry::getDAO('SubmissionDAO'); 
-		$submissionId = $submissionDao->insertObject($submission);
-
+				'editDecisionId' => null,
+				'editorId' => $user->getId(),
+				'decision' => 4,
+				'dateDecided' => strtotime('2021-03-30 16:00:00')
+			);
+			
 		$editDecisionDAO = DAORegistry::getDAO('EditDecisionDAO');
-		$editDecisionDAO->updateEditorDecision($submissionId, $editorDecision);
+		$editDecisionDAO->updateEditorDecision($this->submissionId, $editorDecision);
 
-
-		parent::setUp();
+		$decisionsSubmission = $editDecisionDAO->getEditorDecisions($this->submissionId);
+		self::assertTrue(is_integer($decisionsSubmission[0]['decision']));
 	}
- 
-    public function testReportWithSectionsDecisionDate(){
+
+    public function testFilterByFinalDecisionDate(){
 		
 		$scieloSubmissionsReportDAO =& DAORegistry::getDAO('ScieloSubmissionsReportDAO');
 		$submissionsData = $scieloSubmissionsReportDAO->getReportWithSections($this->application,$this->journalId,null,null,$this->initialDecisionDate,$this->finalDecisionDate,$this->sessions);
