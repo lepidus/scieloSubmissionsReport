dca5e43e8be675724a06ee2e24acee2f80be93c4
aline <aline@lepidus.com.br>
2021-05-07 15:15:28 -0400

    Implements unit test factoring to be specific
    Issue: documentacao-e-tarefas/scielo#153
    
    Signed-off-by: aline <aline@lepidus.com.br>
    Signed-off-by: thiago <thiago@lepidus.com.br>

--- tests/ScieloSubmissionsReportDAOTest.php
+++ tests/ScieloSubmissionsReportDAOTest.php
@@ -1,12 +1,10 @@
 <?php
 
-import('lib.pkp.tests.DatabaseTestCase');
+import('lib.pkp.tests.PKPTestCase');
 import('plugins.reports.ScieloSubmissionsReportPlugin.ScieloSubmissionsReportDAO');
-import('classes.submission.Submission');
-import('classes.publication.Publication');
-import('lib.pkp.classes.user.User');
 
-class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
+
+class ScieloSubmissionsReportDAOTest extends PKPTestCase {
 
 	private $application;
 	private $journalId;
@@ -14,102 +12,38 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
 	private $finalDecisionDate;
 	private $sessions;
 
+			
 	
 	protected function setUp() : void {
 		
 		$this->application = "ops";
 		$this->journalId = 1;
-		$this->initialDecisionDate = "2021-03-01";
-		$this->finalDecisionDate = "2021-05-31";
+		$this->initialDecisionDate = "2020-04-29";
+		$this->finalDecisionDate = "2020-09-07";
 		$this->sessions = ["Health Sciences"];
 		
 		$scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
 		DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
-		
+
+
 		parent::setUp();
 	}
-	
-	public function testCreateSubmission(){
-		// Create Submission
-		$submission = new Submission();
-		$submission->setContextId($this->journalId);
-		$submission->setStatus(STATUS_DECLINED);
-		$submission->setSubmissionProgress(1);
-		$submission->stampStatusModified();
-		$submission->setStageId(STATUS_DECLINED);
-		$submission->setData('dateSubmitted', date('2021-03-15 16:00:00'));
-		
-		// Insert Submission
-		$submissionDao = DAORegistry::getDAO('SubmissionDAO'); 
-		$submissionId = $submissionDao->insertObject($submission);
-		$newSubmission = $submissionDao->getById($submissionId);
-		
-		//Create Publication
-		$publication = new Publication();
-		$this->setPublicationData($publication, $newSubmission);
-		$publication->setData('status', STATUS_QUEUED);
-		$publication->setData('version', 1);
-		$publication->setData('sectionId', 3);
 
-		// Insert publication
-		$publicationDao = DAORegistry::getDAO('PublicationDAO');
-		$publicationId = $publicationDao->insertObject($publication);
-		$newPublication = $publicationDao->getById($publicationId);
-
-		$newSubmission->_data = array_merge($newSubmission->_data, ['currentPublicationId' => $newPublication->getId()]);
-		$submissionDao->updateObject($newSubmission);
-
-		self::assertTrue(is_integer($submissionId));
-		self::assertTrue(is_integer($publicationId));
-		self::assertTrue(is_integer(($submissionDao->getById($submissionId))->getSectionId()));
-
-		return $submissionId;
-	}
-
-	function setPublicationData($publication, $submission) {
-		$locale = $publication->getData('locale');
-		$publication->setData('submissionId', $submission->getId());
-		$publication->setData('locale', $locale);
-		$publication->setData('language', PKPString::substr($locale, 0, 2));
-	}
-
-	/**
-     * @depends testCreateSubmission
-     */
-	public function testEditorDecisionSettings(int $submissionId) {
-		$user = new User();
-		
-		$editorDecision = array(
-				'editDecisionId' => null,
-				'editorId' => $user->getId(),
-				'decision' => 4,
-				'dateDecided' => strtotime('2021-03-30 16:00:00')
-			);
-			
-		$editDecisionDAO = DAORegistry::getDAO('EditDecisionDAO');
-		$editDecisionDAO->updateEditorDecision($submissionId, $editorDecision);
-
-		$decisionsSubmission = $editDecisionDAO->getEditorDecisions($submissionId);
-		self::assertTrue(is_string($decisionsSubmission[0]['decision']));
-	}
 
     public function testFilterByFinalDecisionDate(){
+
 		
 		$scieloSubmissionsReportDAO =& DAORegistry::getDAO('ScieloSubmissionsReportDAO');
 		$submissionsData = $scieloSubmissionsReportDAO->getReportWithSections($this->application,$this->journalId,null,null,$this->initialDecisionDate,$this->finalDecisionDate,$this->sessions);
+
+		$expectedLength = 4;
+		$lengthResult = sizeof($submissionsData);
+
+		$expectedTitle = 'Um estudo sobre a Arte da Guerra';
+		$titleResult = $submissionsData[0][1];
 		
-		for ($index = 0; $index < (sizeof($submissionsData)-3) ; $index++) {
-			if ($submissionsData[$index][15] == '') self::assertTrue(false);
-
-			if ((strtotime($this->initialDecisionDate) > strtotime($submissionsData[$index][15])) ||  (strtotime($submissionsData[$index][15]) > strtotime($this->finalDecisionDate)))
-				self::assertTrue(false);
-		}
-
-		self::assertTrue(true);
-	}
-	
-	protected function getAffectedTables() {
-		return array('submissions','edit_decisions','publications');
+		self::assertEquals($expectedLength, $lengthResult);
+		self::assertEquals($expectedTitle, $titleResult);
 	}
 
 }
