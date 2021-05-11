89e734e207895727ece518b0f7678204cfea36b4
thiagolepidus <thiago@lepidus.com.br>
2021-05-11 14:09:00 -0400

    Performs unit test with the test database.
    Issue: documentacao-e-tarefas/scielo#153
    
    Signed-off-by: aline <aline@lepidus.com.br>
    Signed-off-by: thiago <thiago@lepidus.com.br>

--- tests/ScieloSubmissionsReportDAOTest.php
+++ tests/ScieloSubmissionsReportDAOTest.php
@@ -1,49 +1,78 @@
 <?php
 
-import('lib.pkp.tests.PKPTestCase');
+import('lib.pkp.tests.DatabaseTestCase');
+import('lib.pkp.tests.PKPTestHelper');
 import('plugins.reports.ScieloSubmissionsReportPlugin.ScieloSubmissionsReportDAO');
 
+class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
 
-class ScieloSubmissionsReportDAOTest extends PKPTestCase {
+    private $application;
+    private $journalId;
+    private $initialDecisionDate;
+    private $finalDecisionDate;
+    private $sessions;
+    private $dbDumpPath;
 
-	private $application;
-	private $journalId;
-	private $initialDecisionDate;
-	private $finalDecisionDate;
-	private $sessions;
+    protected function setUp() : void {
+        
+        $this->application = "ops";
+        $this->journalId = 1;
+        $this->initialDecisionDate = "2020-04-29";
+        $this->finalDecisionDate = "2020-09-07";
+        $this->sessions = ["Health Sciences"];
+        $this->dbDumpPath = Core::getBaseDir() . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, ['plugins','reports','ScieloSubmissionsReportPlugin','tests','dbdump']);
+        
+        $scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
+        DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
 
-			
-	
-	protected function setUp() : void {
-		
-		$this->application = "ops";
-		$this->journalId = 1;
-		$this->initialDecisionDate = "2020-04-29";
-		$this->finalDecisionDate = "2020-09-07";
-		$this->sessions = ["Health Sciences"];
-		
-		$scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
-		DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
+		$this->getDumpAplication();
 
+        putenv('DATABASEDUMP=' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'scieloSubmissionsReportTest.sql.gz');
+        PKPTestHelper::restoreDB($this);
+        
+        parent::setUp();
+    }
 
-		parent::setUp();
+	public function getDumpAplication(){
+
+		exec('/usr/bin/mysqldump --user=' .
+            escapeshellarg(Config::getVar('database', 'username')) .
+            ' --password=' .
+            escapeshellarg(Config::getVar('database', 'password')) .
+            ' --host=' .
+            escapeshellarg(Config::getVar('database', 'host')) .
+            ' ' .
+            escapeshellarg(Config::getVar('database', 'name')) .
+			' > ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql' . 
+			'| zip ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip ' .
+			$this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql'
+		);
 	}
 
+    protected function getAffectedTables(){
+        return PKP_TEST_ENTIRE_DB;
+    }
 
     public function testFilterByFinalDecisionDate(){
+        
+        $scieloSubmissionsReportDAO =& DAORegistry::getDAO('ScieloSubmissionsReportDAO');
+        $submissionsData = $scieloSubmissionsReportDAO->getReportWithSections(
+            $this->application,
+            $this->journalId,
+            null,
+            null,
+            $this->initialDecisionDate,
+            $this->finalDecisionDate,
+            $this->sessions
+        );
 
-		
-		$scieloSubmissionsReportDAO =& DAORegistry::getDAO('ScieloSubmissionsReportDAO');
-		$submissionsData = $scieloSubmissionsReportDAO->getReportWithSections($this->application,$this->journalId,null,null,$this->initialDecisionDate,$this->finalDecisionDate,$this->sessions);
+        $expectedLength = 4;
+        $lengthResult = sizeof($submissionsData);
 
-		$expectedLength = 4;
-		$lengthResult = sizeof($submissionsData);
-
-		$expectedTitle = 'Um estudo sobre a Arte da Guerra';
-		$titleResult = $submissionsData[0][1];
-		
-		self::assertEquals($expectedLength, $lengthResult);
-		self::assertEquals($expectedTitle, $titleResult);
-	}
-
+        $expectedTitle = 'Um estudo sobre a Arte da Guerra';
+        $titleResult = $submissionsData[0][1];
+      
+        self::assertEquals($expectedLength, $lengthResult);
+        self::assertEquals($expectedTitle, $titleResult);
+    }
 }
