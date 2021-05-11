bfcbb0eacb94c82c1079ca6f4aa85fb37c9676e5
thiagolepidus <thiago@lepidus.com.br>
2021-05-11 15:23:28 -0400

    Restores application database at the end of the unit test.
    Issue: documentacao-e-tarefas/scielo#153
    
    Signed-off-by: aline <aline@lepidus.com.br>
    Signed-off-by: thiago <thiago@lepidus.com.br>

--- tests/ScieloSubmissionsReportDAOTest.php
+++ tests/ScieloSubmissionsReportDAOTest.php
@@ -1,7 +1,7 @@
 <?php
 
-import('lib.pkp.tests.DatabaseTestCase');
 import('lib.pkp.tests.PKPTestHelper');
+import('lib.pkp.tests.DatabaseTestCase');
 import('plugins.reports.ScieloSubmissionsReportPlugin.ScieloSubmissionsReportDAO');
 
 class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
@@ -10,17 +10,21 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
     private $journalId;
     private $initialDecisionDate;
     private $finalDecisionDate;
-    private $sessions;
+    private $sections;
     private $dbDumpPath;
 
     protected function setUp() : void {
-        
         $this->application = "ops";
         $this->journalId = 1;
         $this->initialDecisionDate = "2020-04-29";
         $this->finalDecisionDate = "2020-09-07";
-        $this->sessions = ["Health Sciences"];
-        $this->dbDumpPath = Core::getBaseDir() . DIRECTORY_SEPARATOR . join(DIRECTORY_SEPARATOR, ['plugins','reports','ScieloSubmissionsReportPlugin','tests','dbdump']);
+        $this->sections = ["Health Sciences"];
+        $this->dbDumpPath = 
+            Core::getBaseDir() .
+            DIRECTORY_SEPARATOR .
+            join(DIRECTORY_SEPARATOR, 
+                ['plugins','reports','ScieloSubmissionsReportPlugin','tests','dbdump']
+            );
         
         $scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
         DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
@@ -29,12 +33,11 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
 
         putenv('DATABASEDUMP=' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'scieloSubmissionsReportTest.sql.gz');
         PKPTestHelper::restoreDB($this);
-        
+
         parent::setUp();
     }
 
 	public function getDumpAplication(){
-
 		exec('/usr/bin/mysqldump --user=' .
             escapeshellarg(Config::getVar('database', 'username')) .
             ' --password=' .
@@ -43,10 +46,10 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
             escapeshellarg(Config::getVar('database', 'host')) .
             ' ' .
             escapeshellarg(Config::getVar('database', 'name')) .
-			' > ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql' . 
-			'| zip ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip ' .
-			$this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql'
+			' > ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql'
 		);
+        exec('zip ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip ' .
+        $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql');
 	}
 
     protected function getAffectedTables(){
@@ -54,7 +57,6 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
     }
 
     public function testFilterByFinalDecisionDate(){
-        
         $scieloSubmissionsReportDAO =& DAORegistry::getDAO('ScieloSubmissionsReportDAO');
         $submissionsData = $scieloSubmissionsReportDAO->getReportWithSections(
             $this->application,
@@ -63,16 +65,22 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
             null,
             $this->initialDecisionDate,
             $this->finalDecisionDate,
-            $this->sessions
+            $this->sections
         );
 
         $expectedLength = 4;
         $lengthResult = sizeof($submissionsData);
-
         $expectedTitle = 'Um estudo sobre a Arte da Guerra';
         $titleResult = $submissionsData[0][1];
       
         self::assertEquals($expectedLength, $lengthResult);
         self::assertEquals($expectedTitle, $titleResult);
+    }
+
+    protected function tearDown() : void {
+        putenv('DATABASEDUMP=' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip');
+        parent::tearDown();
+		unlink($this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql');
+		unlink($this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip');
     }
 }
