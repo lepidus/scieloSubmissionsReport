a97548c6dd04a5974ce7627bc7da3859f4e57f38
thiagolepidus <thiago@lepidus.com.br>
2021-05-17 14:00:33 -0400

    Code refactoring according to revision.
    Issue: documentacao-e-tarefas/scielo#153
    
    Signed-off-by: aline <aline@lepidus.com.br>
    Signed-off-by: thiago <thiago@lepidus.com.br>

--- tests/ScieloSubmissionsReportDAOTest.php
+++ tests/ScieloSubmissionsReportDAOTest.php
@@ -4,32 +4,39 @@ import('lib.pkp.tests.PKPTestHelper');
 import('lib.pkp.tests.DatabaseTestCase');
 import('plugins.reports.ScieloSubmissionsReportPlugin.ScieloSubmissionsReportDAO');
 
-class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
+
+class ScieloSubmissionsReportDAOTest extends DatabaseTestCase{
 
     private $application;
     private $journalId;
+    private $initialSubmissionDate;
+    private $finalSubmissionDate;
     private $initialDecisionDate;
     private $finalDecisionDate;
     private $sections;
     private $dbDumpPath;
 
-    protected function setUp() : void {
+    protected function setUp(): void
+    {
         $this->application = "ops";
         $this->journalId = 1;
+        $this->initialSubmissionDate = null;
+        $this->finalSubmissionDate = null;
         $this->initialDecisionDate = "2020-04-29";
         $this->finalDecisionDate = "2020-09-07";
         $this->sections = ["Health Sciences"];
-        $this->dbDumpPath = 
-            Core::getBaseDir() .
+
+        $this->dbDumpPath =
+        Core::getBaseDir() .
             DIRECTORY_SEPARATOR .
             join(DIRECTORY_SEPARATOR, 
                 ['plugins','reports','ScieloSubmissionsReportPlugin','tests','dbdump']
             );
-        
+
         $scieloSubmissionsReportDAO = new ScieloSubmissionsReportDAO();
         DAORegistry::registerDAO('ScieloSubmissionsReportDAO', $scieloSubmissionsReportDAO);
 
-		$this->getDumpAplication();
+        $this->backupCurrentDatabase();
 
         putenv('DATABASEDUMP=' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'scieloSubmissionsReportTest.sql.gz');
         PKPTestHelper::restoreDB($this);
@@ -37,8 +44,9 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
         parent::setUp();
     }
 
-	public function getDumpAplication(){
-		exec('/usr/bin/mysqldump --user=' .
+    public function backupCurrentDatabase()
+    {
+        exec('/usr/bin/mysqldump --user=' .
             escapeshellarg(Config::getVar('database', 'username')) .
             ' --password=' .
             escapeshellarg(Config::getVar('database', 'password')) .
@@ -46,41 +54,46 @@ class ScieloSubmissionsReportDAOTest extends DatabaseTestCase {
             escapeshellarg(Config::getVar('database', 'host')) .
             ' ' .
             escapeshellarg(Config::getVar('database', 'name')) .
-			' > ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql'
-		);
+            ' > ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql'
+        );
         exec('zip ' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip ' .
-        $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql');
-	}
+            $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql');
+    }
 
-    protected function getAffectedTables(){
+    protected function getAffectedTables()
+    {
         return PKP_TEST_ENTIRE_DB;
     }
 
-    public function testFilterByFinalDecisionDate(){
-        $scieloSubmissionsReportDAO =& DAORegistry::getDAO('ScieloSubmissionsReportDAO');
+    public function testOnlySubmissionsWithFinalDecisionBetweenTheIntervalShoudBeListed()
+    {
+        $scieloSubmissionsReportDAO = &DAORegistry::getDAO('ScieloSubmissionsReportDAO');
         $submissionsData = $scieloSubmissionsReportDAO->getReportWithSections(
             $this->application,
             $this->journalId,
-            null,
-            null,
+            $this->initialSubmissionDate,
+            $this->finalSubmissionDate,
             $this->initialDecisionDate,
             $this->finalDecisionDate,
             $this->sections
         );
 
-        $expectedLength = 4;
-        $lengthResult = sizeof($submissionsData);
+        $fieldsInformationsAboutFilter = 3;
+
+        $expectedNumberOfSubmissions = 1;
+        $resultedNumberOfSubmissions = sizeof($submissionsData) - $fieldsInformationsAboutFilter;
         $expectedTitle = 'Um estudo sobre a Arte da Guerra';
         $titleResult = $submissionsData[0][1];
-      
-        self::assertEquals($expectedLength, $lengthResult);
+
+        self::assertEquals($expectedNumberOfSubmissions, $resultedNumberOfSubmissions);
         self::assertEquals($expectedTitle, $titleResult);
     }
 
-    protected function tearDown() : void {
+    protected function tearDown(): void
+    {
         putenv('DATABASEDUMP=' . $this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip');
         parent::tearDown();
-		unlink($this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql');
-		unlink($this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip');
+        unlink($this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.sql');
+        unlink($this->dbDumpPath . DIRECTORY_SEPARATOR . 'dumpApplication.zip');
     }
 }
