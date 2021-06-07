26e0c07a0cb32769de5fe167264787dff61a7d7d
Jhon <jhon@lepidus.com.br>
2021-06-07 18:39:27 -0400

    Creates class for closed date intervals
    Issue: documentacao-e-tarefas/scielo#173
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/ClosedDateInterval.inc.php
@@ -0,0 +1,25 @@
+<?php
+
+class ClosedDateInterval {
+    private $beginningDate;
+    private $endDate;
+
+    public function __construct(string $beginningDate, string $endDate) {
+        $this->beginningDate = new DateTime($beginningDate . " 00:00:00");
+        $this->endDate = new DateTime($endDate . " 23:59:59");
+    }
+
+    public function getBeginningDate(): string {
+        return $this->beginningDate->format('Y-m-d H:i:s');
+    }
+
+    public function getEndDate(): string {
+        return $this->endDate->format('Y-m-d H:i:s');
+    }
+
+    public function isInsideInterval(string $date): bool {
+        $datetime = new DateTime($date);
+
+        return ($this->beginningDate <= $datetime) && ($datetime <= $this->endDate);
+    }
+}
