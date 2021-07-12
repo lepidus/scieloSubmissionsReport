42ee3d963f6c6277c8caff93cebcd1a8c55dfb3b
Jhon <jhon@lepidus.com.br>
2021-07-12 15:01:00 -0400

    Replaces hardcoded headers by localized ones
    Issue: documentacao-e-tarefas/scielo#176
    
    Signed-off-by: Iudi <iudi@lepidus.com.br>
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- classes/ScieloSubmissionsOPSReport.inc.php
+++ classes/ScieloSubmissionsOPSReport.inc.php
@@ -5,6 +5,25 @@ import('plugins.reports.scieloSubmissionsReport.classes.ScieloSubmissionsReport'
 class ScieloSubmissionsOPSReport extends ScieloSubmissionsReport {
 
     protected function getHeaders() : array {
-        return ["ID da submissão","Título da Submissão","Submetido por","Data de submissão","Dias até mudança de status","Estado da submissão","Moderador de área","Moderadores","Autores","Seção","Idioma","Estado de publicação","DOI da publicação","Notas","Decisão final","Data da decisão final","Tempo em avaliação","Tempo entre submissão e decisão final"];
+        return [
+            __("plugins.reports.scieloSubmissionsReport.header.submissionId"),
+            __("submission.submissionTitle"),
+            __("submission.submitter"),
+            __("common.dateSubmitted"),
+            __("plugins.reports.scieloSubmissionsReport.header.daysChangeStatus"),
+			__("plugins.reports.scieloSubmissionsReport.header.submissionStatus"),
+            __("plugins.reports.scieloSubmissionsReport.header.areaModerator"),
+			__("plugins.reports.scieloSubmissionsReport.header.moderators"),
+            __("submission.authors"),
+            __("plugins.reports.scieloSubmissionsReport.header.section"),
+            __("common.language"),
+            __("plugins.reports.scieloSubmissionsReport.header.publicationStatus"),
+			__("plugins.reports.scieloSubmissionsReport.header.publicationDOI"),
+            __("submission.notes"),
+            __("plugins.reports.scieloSubmissionsReport.header.FinalDecision"),
+            __("plugins.reports.scieloSubmissionsReport.header.finalDecisionDate"),
+            __("plugins.reports.scieloSubmissionsReport.header.ReviewingTime"),
+			__("plugins.reports.scieloSubmissionsReport.header.SubmissionAndFinalDecisionDateInterval")
+        ];
     }
 }
