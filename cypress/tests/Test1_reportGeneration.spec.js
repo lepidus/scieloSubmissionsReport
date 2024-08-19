f28900ed8387775f1b7b0fb195566a8ced9a30b0
Jhon <jhon@lepidus.com.br>
2024-08-19 14:11:05 -0400

    Integration test - Field to select all sections
    Issue: documentacao-e-tarefas/scielo#694
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_reportGeneration.spec.js
+++ cypress/tests/Test1_reportGeneration.spec.js
@@ -76,4 +76,39 @@ describe("SciELO Submissions Report - Report generation", function() {
             expect(text).to.contain('Articles,Reviews');
         });
     });
+    it('Generates CSV using field to select all sections', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+
+        cy.contains('a.app__navItem', 'Reports').click();
+        cy.contains('a', 'SciELO Submissions Report').click();
+
+        cy.get('#selectFilterTypeDate').select('Filter by submitted date');
+        cy.get('#selectAllSections').check();
+        cy.contains('label', 'Articles').parent().within(() => {
+            cy.get('input').should('be.checked');
+        });
+        cy.contains('label', 'Reviews').parent().within(() => {
+            cy.get('input').should('be.checked');
+        });
+
+        cy.get('#selectAllSections').uncheck();
+        cy.contains('label', 'Articles').parent().within(() => {
+            cy.get('input').should('not.be.checked');
+        });
+        cy.contains('label', 'Reviews').parent().within(() => {
+            cy.get('input').should('not.be.checked');
+        });
+        
+        cy.get('#selectAllSections').check();
+        cy.contains('Generate Report').click();
+        cy.wait(2000);
+
+        let now = getNowDateAndHour();
+        const downloadsFolder = Cypress.config('downloadsFolder');
+        const reportFileName = 'submissionsJPKJPK-' + now + '.csv';
+
+        cy.readFile(downloadsFolder + reportFileName, 'utf-8').then((text) => {
+            expect(text).to.contain('Articles,Reviews');
+        });
+    });
 });
