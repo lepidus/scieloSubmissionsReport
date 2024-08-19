9ca2e553e3cbaa671e3b6b7c71f2415df8820f7a
Jhon <jhon@lepidus.com.br>
2024-08-19 12:40:06 -0400

    Integration test - Check generation of report
    Issue: documentacao-e-tarefas/scielo#694
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_reportGeneration.spec.js
+++ cypress/tests/Test1_reportGeneration.spec.js
@@ -1,5 +1,28 @@
+function getNowDateAndHour() {
+    let now = new Date().toISOString();
+    const charactersToRemove = ['-', ':', 'T'];
+    let nowFormatted = '';
+
+    for (let i = 0; i < now.length; i++) {
+        let shouldRemove = false;
+        
+        for (let j = 0; j < charactersToRemove.length; j++) {
+            if (now[i] === charactersToRemove[j]) {
+                shouldRemove = true;
+                break;
+            }
+        }
+
+        if (!shouldRemove) {
+            nowFormatted += now[i];
+        }
+    }
+
+    return (nowFormatted.split('.')[0]);
+}
+
 describe("SciELO Submissions Report - Report generation", function() {
-    it("Generates report", function() {
+    it("Asserts presence of report setting fields", function() {
         cy.login('dbarnes', null, 'publicknowledge');
 
         cy.contains('a.app__navItem', 'Reports').click();
@@ -27,5 +50,30 @@ describe("SciELO Submissions Report - Report generation", function() {
 
         cy.contains('The report generation proccesss can take a few minutes, depending on the parameters selected and the number of submissions present in the system.');
         cy.contains('input', 'Generate Report');
+    });
+    it('Generates CSV report', function () {
+        cy.login('dbarnes', null, 'publicknowledge');
+
+        cy.contains('a.app__navItem', 'Reports').click();
+        cy.contains('a', 'SciELO Submissions Report').click();
+
+        cy.get('#selectFilterTypeDate').select('Filter by submitted date');
+        cy.contains('label', 'Articles').parent().within(() => {
+            cy.get('input').check();
+        });
+        cy.contains('label', 'Reviews').parent().within(() => {
+            cy.get('input').check();
+        });
+
+        cy.contains('Generate Report').click();
+        cy.wait(2000);
+
+        let now = getNowDateAndHour();
+        const downloadsFolder = Cypress.config('downloadsFolder');
+        const reportFileName = 'submissionsJPKJPK-' + now + '.csv';
+
+        cy.readFile(downloadsFolder + reportFileName, 'utf-8').then((text) => {
+            expect(text).to.contain('Articles,Reviews');
+        });
     });
 });
