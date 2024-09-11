1a6bf95dd2842ec9ce0dec1fe5fc4d0886b5ef6e
yves <yves@lepidus.com.br>
2024-09-11 18:06:31 -0400

    Update ScieloPreprint.php file

--- classes/ScieloPreprint.php
+++ classes/ScieloPreprint.php
@@ -28,7 +28,7 @@ class ScieloPreprint extends ScieloSubmission
     {
         $record = [$this->id, $this->title, $this->submitter, $this->submitterCountry, $this->getSubmitterIsScieloJournal(), $this->dateSubmitted, $this->daysUntilStatusChange, $this->status, $this->getSectionModerators(), $this->getResponsibles(), $this->authorsAsRecord(), $this->section, $this->language, $this->getPublicationStatus(), $this->getPublicationDOI(), $this->getNotes(), $this->finalDecision, $this->finalDecisionDate, $this->getTimeUnderReview(), $this->getTimeBetweenSubmissionAndFinalDecision()];
 
-        if(!is_null($this->stats)) {
+        if (!is_null($this->stats)) {
             $record = array_merge($record, $this->stats->asRecord());
         }
 
@@ -66,7 +66,7 @@ class ScieloPreprint extends ScieloSubmission
 
     public function getNotes(): string
     {
-        if(empty($this->notes)) {
+        if (empty($this->notes)) {
             return __('plugins.reports.scieloSubmissionsReport.warning.noNotes');
         }
 
