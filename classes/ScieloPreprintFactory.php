5b13d542714020f9db3d88ee0620e494dd53cf93
Jhon <jhon@lepidus.com.br>
2026-03-31 14:54:51 -0400

    Refactors preprint creations
    Issue: documentacao-e-tarefas/scielo#893
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Carlos <carlos@lepidus.com.br>

--- classes/ScieloPreprintFactory.php
+++ classes/ScieloPreprintFactory.php
@@ -22,6 +22,7 @@ class ScieloPreprintFactory extends ScieloSubmissionFactory
         $submitter = $this->retrieveSubmitter($submissionId);
         $submitterCountry = $this->retrieveSubmitterCountry($submissionId);
         $submitterIsScieloJournal = $this->retrieveSubmitterIsScieloJournal($submissionId);
+        $doi = $scieloPreprintsDAO->getPublicationDoi($publicationId);
         $dateSubmitted = $submission['date_submitted'];
         $daysUntilStatusChange = $this->calculateDaysUntilStatusChange($dateSubmitted, $submission['date_last_activity']);
         $status = $this->getStatusMessage($submission['status']);
@@ -48,6 +49,7 @@ class ScieloPreprintFactory extends ScieloSubmissionFactory
             $submitter,
             $submitterCountry,
             $submitterIsScieloJournal,
+            $doi,
             $dateSubmitted,
             $daysUntilStatusChange,
             $status,
--- tests/ScieloSubmissionsOPSReportTest.php
+++ tests/ScieloSubmissionsOPSReportTest.php
@@ -55,9 +55,9 @@ class ScieloSubmissionsOPSReportTest extends TestCase
         $finalDecisionDateForPreprint3 = '2020-11-15';
         $stats = new SubmissionStats(10, 10);
 
-        $preprint1 = new ScieloPreprint(1, 'Title 1', 'Paola Franchesca', 'Brasil', false, $submittedDateForPreprint1, 1, 'Posted', [new SubmissionAuthor('Paola Franchesca', 'Italy', 'University of Milan')], 'Fashion Design', 'en', 'Accepted', $finalDecisionDateForPreprint1, ['Jean Paul Cardin'], ['Jean Paul Cardin'], 'Sent to journal publication', 'No DOI informed', [''], $stats);
-        $preprint2 = new ScieloPreprint(2, 'Titulo 2', 'Pablo Giorgio', 'Brasil', false, $submittedDateForPreprint2, 6, 'Posted', [new SubmissionAuthor('Atila', 'Brazil', 'USP')], 'Biological', 'en', 'Accepted', $finalDecisionDateForPreprint2, ['Richard Feynman'], ['Neil Tyson'], 'Sent to journal publication', 'No DOI informed', [''], $stats);
-        $preprint3 = new ScieloPreprint(3, 'Titulo 3', 'Pablo Giorgio', 'Brasil', false, $submittedDateForPreprint3, 6, 'Posted', [new SubmissionAuthor('Atila', 'Brazil', 'USP')], 'Biological', 'en', 'Accepted', $finalDecisionDateForPreprint3, ['Richard Feynman'], ['Neil Tyson'], 'Sent to journal publication', 'No DOI informed', [''], $stats);
+        $preprint1 = new ScieloPreprint(1, 'Title 1', 'Paola Franchesca', 'Brasil', false, '10.12334/TestPreprint.1', $submittedDateForPreprint1, 1, 'Posted', [new SubmissionAuthor('Paola Franchesca', 'Italy', 'University of Milan')], 'Fashion Design', 'en', 'Accepted', $finalDecisionDateForPreprint1, ['Jean Paul Cardin'], ['Jean Paul Cardin'], 'Sent to journal publication', 'No DOI informed', [''], $stats);
+        $preprint2 = new ScieloPreprint(2, 'Titulo 2', 'Pablo Giorgio', 'Brasil', false, '10.12334/TestPreprint.2', $submittedDateForPreprint2, 6, 'Posted', [new SubmissionAuthor('Atila', 'Brazil', 'USP')], 'Biological', 'en', 'Accepted', $finalDecisionDateForPreprint2, ['Richard Feynman'], ['Neil Tyson'], 'Sent to journal publication', 'No DOI informed', [''], $stats);
+        $preprint3 = new ScieloPreprint(3, 'Titulo 3', 'Pablo Giorgio', 'Brasil', false, '10.12334/TestPreprint.3', $submittedDateForPreprint3, 6, 'Posted', [new SubmissionAuthor('Atila', 'Brazil', 'USP')], 'Biological', 'en', 'Accepted', $finalDecisionDateForPreprint3, ['Richard Feynman'], ['Neil Tyson'], 'Sent to journal publication', 'No DOI informed', [''], $stats);
 
         return [$preprint1, $preprint2, $preprint3];
     }
