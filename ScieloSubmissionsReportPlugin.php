d32b736c2fde167bff7b57f290f9ad1a0f82edae
Jhon <jhon@lepidus.com.br>
2026-09-02 17:37:41 -0400

    Improves validation code in form class
    Issue: documentacao-e-tarefas/scielo#948
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- ScieloSubmissionsReportPlugin.php
+++ ScieloSubmissionsReportPlugin.php
@@ -70,9 +70,8 @@ class ScieloSubmissionsReportPlugin extends ReportPlugin
         $form = new ScieloSubmissionsReportForm($this);
         $form->initData();
         if ($request->isPost($request)) {
-            $reportParams = $request->getUserVars();
-            $validationResult = $form->validateReportData($reportParams);
-            if ($validationResult) {
+            $form->readInputData();
+            if ($form->validate()) {
                 $form->generateReport($request);
             }
         } else {
